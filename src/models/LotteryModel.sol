a12955b50d9c4a7cf167d9ebdeecf1f5ebf5bc80
Albert Puigsech <albert@puigsech.com>
2023-08-01 22:17:41 +0200

    define initial version for the models (lottery, ticket)

--- src/models/LotteryModel.sol
+++ src/models/LotteryModel.sol
@@ -3,6 +3,31 @@ pragma solidity ^0.8.19;
 
 library LotteryModel {
     struct LotteryItem{
-        uint dummy;
+        string Name;                    // Human-readable identifier for the lottery.
+
+        uint256 InitBlock;              // Block number at which the lottery rounds are initialized or started.
+        uint32 Rounds;                  // Number of rounds or iterations for the lottery (many times the lottery will be played).
+        uint16 RoundBlocks;             // Number of blocks between each round.
+
+        uint256 BetPrice;               // Cost of a single bet for the lottery.
+
+        uint256 JackpotMin;             // Minimum size of the lottery jackpot.
     }
+
+    function isValid(LotteryModel.LotteryItem calldata lottery) 
+        internal pure
+    {}
+
+    function newEmptyLottery()
+        internal pure
+        returns(LotteryModel.LotteryItem memory lottery) 
+    {
+        lottery.Name = "";
+        lottery.InitBlock = 0;
+        lottery.Rounds = 0;
+        lottery.RoundBlocks = 0;
+        lottery.BetPrice = 0;
+        lottery.JackpotMin = 0;
+    }
+
 }
--- src/models/TicketModel.sol
+++ src/models/TicketModel.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.8.19;
 
 library TicketModel {
     struct TicketItem{
-        uint dummy;
+        uint32 LotteryID;                   // Reference identifier of the lottery associated with the ticket.
+        uint32 LotteryRoundInit;            // Starting round of the lottery for which the ticket is playing.
+        uint32 LotteryRoundFini;            // Ending round of the lottery for which the ticket is playing.
     }
+
+    function isValid(TicketModel.TicketItem calldata ticket) 
+        internal pure
+    {}
+
+    function newEmptyTicket()
+        internal pure
+        returns(TicketModel.TicketItem memory ticket)
+    {
+        ticket.LotteryID = 0;
+        ticket.LotteryRoundInit = 0;
+        ticket.LotteryRoundFini = 0;
+    }
+
 }
