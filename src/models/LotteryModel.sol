0e8a2c8489a7fc34f6da544ebd8389070968f1a1
Albert Puigsech <albert@puigsech.com>
2023-08-02 11:33:29 +0200

    comment and document code

--- src/models/LotteryModel.sol
+++ src/models/LotteryModel.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/// @title Library containing the data model and functions related to the LotteryItem struct.
 library LotteryModel {
+    // Custom errors used for validation.
+    /// @dev Error thrown when the name of the lottery is invalid (empty or non-existent).
     error InvalidName();
+    /// @dev Error thrown when the rounds configuration of the lottery is invalid (zero rounds or round blocks).
     error InvalidRoundsConfiguration();
 
-    struct LotteryItem{
+    /// @dev Data structure representing a specific lottery.
+    struct LotteryItem {
         string Name;                    // Human-readable identifier for the lottery.
 
         uint256 InitBlock;              // Block number at which the lottery rounds are initialized or started.
 
-        uint32 Rounds;                  // Number of rounds or iterations for the lottery (many times the lottery will be played).
+        uint32 Rounds;                  // Number of rounds or iterations for the lottery (how many times the lottery will be played).
         uint16 RoundBlocks;             // Number of blocks between each round.
 
         uint256 BetPrice;               // Cost of a single bet for the lottery.
@@ -18,16 +23,18 @@ library LotteryModel {
         uint256 JackpotMin;             // Minimum size of the lottery jackpot.
     }
 
+    /// @dev Function to validate whether a lottery item is valid (has a name and valid rounds configuration).
     function isValid(LotteryModel.LotteryItem memory lottery) 
         internal pure
     {
-        if (bytes(lottery.Name).length == 0) { revert InvalidName(); }
-        if (lottery.Rounds == 0 || lottery.RoundBlocks == 0) { revert InvalidRoundsConfiguration(); }
+        if (bytes(lottery.Name).length == 0) revert InvalidName();
+        if (lottery.Rounds == 0 || lottery.RoundBlocks == 0) revert InvalidRoundsConfiguration();
     }
 
+    /// @dev Function to create an empty LotteryItem.
     function newEmptyLottery()
         internal pure
-        returns(LotteryModel.LotteryItem memory lottery) 
+        returns(LotteryModel.LotteryItem memory lottery)
     {
         lottery.Name = "";
         lottery.InitBlock = 0;
@@ -38,43 +45,50 @@ library LotteryModel {
     }
 }
 
-
+/// @title Library containing functions to manage the storage of LotteryItems.
 library LotteryModelStorage {
     using LotteryModel for LotteryModel.LotteryItem;
 
+    // Custom error used when an invalid ID is encountered.
+    /// @dev Error thrown when an invalid lottery ID is encountered.
     error InvalidID();
 
+    /// @dev Data structure to store multiple lottery items.
     struct LotteryStorage {
         mapping (uint32 => LotteryModel.LotteryItem) LotteryMap;
     }
 
+    /// @dev Function to set (update/create) a lottery item in the storage.
     function set(LotteryStorage storage data, uint32 id, LotteryModel.LotteryItem calldata lottery)
         internal
         isValid(lottery)
     {
-        data.LotteryMap[id] = lottery;        
+        data.LotteryMap[id] = lottery;
     }
 
+    /// @dev Function to unset (delete) a lottery item from the storage.
     function unset(LotteryStorage storage data, uint32 id)
         internal
     {
         delete data.LotteryMap[id];
     }
 
+    /// @dev Function to get a specific lottery item from the storage by its ID.
     function get(LotteryStorage storage data, uint32 id)
         internal view
         exist(data, id)
         returns (LotteryModel.LotteryItem storage lottery)
     {
         lottery = data.LotteryMap[id];
-
     }
 
+    /// @dev Modifier to check if a lottery item with a given ID exists in the storage.
     modifier exist(LotteryStorage storage data, uint32 id) {
-        if (data.LotteryMap[id].Rounds == 0) { revert InvalidID(); }
+        if (data.LotteryMap[id].Rounds == 0) revert InvalidID();
         _;
     }
 
+    /// @dev Modifier to check if a LotteryItem calldata is valid.
     modifier isValid(LotteryModel.LotteryItem calldata lottery) {
         LotteryModel.isValid(lottery);
         _;
--- src/models/TicketModel.sol
+++ src/models/TicketModel.sol
@@ -1,21 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/// @title Library containing the data model and functions related to the TicketItem struct.
 library TicketModel {
+    // Custom errors used for validation.
+    /// @dev Error thrown when the ticket's round configuration is invalid (round initialization is zero or ending round is less than the starting round).
     error InvalidRounds();
 
+    /// @dev Data structure representing a lottery ticket.
     struct TicketItem{
         uint32 LotteryID;                   // Reference identifier of the lottery associated with the ticket.
         uint32 LotteryRoundInit;            // Starting round of the lottery for which the ticket is playing.
         uint32 LotteryRoundFini;            // Ending round of the lottery for which the ticket is playing.
     }
 
+    /// @dev Function to validate whether a ticket item is valid (has valid round configuration).
     function isValid(TicketModel.TicketItem memory ticket) 
         internal pure
     {
-        if (ticket.LotteryRoundInit == 0 || ticket.LotteryRoundFini < ticket.LotteryRoundInit ) { revert InvalidRounds(); }
+        if (ticket.LotteryRoundInit == 0 || ticket.LotteryRoundFini < ticket.LotteryRoundInit ) revert InvalidRounds();
     }
 
+    /// @dev Function to create an empty TicketItem.
     function newEmptyTicket()
         internal pure
         returns(TicketModel.TicketItem memory ticket)
@@ -26,15 +32,20 @@ library TicketModel {
     }
 }
 
+/// @title Library containing functions to manage the storage of TicketItems.
 library TicketModelStorage {
     using TicketModel for TicketModel.TicketItem;
 
+    // Custom error used when an invalid ID is encountered.
+    /// @dev Error thrown when an invalid ticket ID is encountered.
     error InvalidID();
 
+    /// @dev Data structure to store multiple ticket items.
     struct TicketStorage {
         mapping (uint32 => TicketModel.TicketItem) TicketMap;
     }
 
+    /// @dev Function to set (update/create) a ticket item in the storage.
     function set(TicketStorage storage data, uint32 id, TicketModel.TicketItem calldata ticket)
         internal
         isValid(ticket)
@@ -42,12 +53,14 @@ library TicketModelStorage {
         data.TicketMap[id] = ticket;        
     }
 
+    /// @dev Function to unset (delete) a ticket item from the storage.
     function unset(TicketStorage storage data, uint32 id)
         internal
     {
         delete data.TicketMap[id];
     }
 
+    /// @dev Function to get a specific ticket item from the storage by its ID.
     function get(TicketStorage storage data, uint32 id)
         internal view
         exist(data, id)
@@ -57,11 +70,13 @@ library TicketModelStorage {
 
     }
 
+    /// @dev Modifier to check if a ticket item with a given ID exists in the storage.
     modifier exist(TicketStorage storage data, uint32 id) {
-        if (data.TicketMap[id].LotteryRoundInit == 0) { revert InvalidID(); }
+        if (data.TicketMap[id].LotteryRoundInit == 0) revert InvalidID();
         _;
     }
 
+    /// @dev Modifier to check if a TicketItem is valid.
     modifier isValid(TicketModel.TicketItem calldata ticket) {
         TicketModel.isValid(ticket);
         _;
