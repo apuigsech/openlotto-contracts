42fee20f3991d1441f9249c369db907d01f33224
Albert Puigsech <albert@puigsech.com>
2023-09-14 19:31:19 +0200

    implememnt deployment with sample data for testing purposes

---
+++ script/DeployOpenLottoWithSampleData.s.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "@src/utils/Deployments.sol";
+import "@src/operators/BaseLotteryOperator.sol";
+
+contract DummyLotteryOperator is BaseLotteryOperator {
+}
+
+
+contract DeployOpenLotto is Script {
+    uint256 internal adminPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+    uint256 internal lotteryManagerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 1);
+    address internal lotteryManagerAddress = vm.addr(lotteryManagerPrivateKey);
+
+    function run() public {
+        uint256 adminPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+        uint256 lotteryManagerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 1);
+        address lotteryManagerAddress = vm.addr(lotteryManagerPrivateKey);
+
+        vm.startBroadcast(adminPrivateKey);
+        OpenLotto openlotto = Deployments.deployAll(lotteryManagerAddress);
+
+        DummyLotteryOperator dummyLotteryOperator = new DummyLotteryOperator();
+        dummyLotteryOperator.grantRole(dummyLotteryOperator.OPERATOR_CONTROLER_ROLE(), address(openlotto));
+
+        vm.stopBroadcast();
+    }
+}
